ea43af598675f0fc1ae6b1ce4e31e6d3e62d988e
Eduardo Pires Martins <edupiresmartins@gmail.com>
2026-06-08 20:14:00 -0300

    configuração inicial do projeto

---
+++ src/main/java/br/com/eduardo/jwtvalidator/JwtValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.jwtvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtValidatorApiApplication.class, args);
+	}
+
+}
